cd427c2a8f015f95753dea722a2daff6d796afee
Siphesihle Mhlongo <sihlecodes@gmail.com>
2025-09-05 10:23:10 +0200

    Use js-yaml over yaml

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
 
-const YAML = require('yaml');
-
+const YAML = require('js-yaml');
 const DOM = require('fauxdom');
 
 const document = new DOM('<!DOCTYPE html><head><style>td { border: 1px solid black; }</style></head><body></body>');
@@ -14,10 +13,10 @@ const columns = 2, rows = 4;
 const flashCardFronts = document.createElement('table');
 
 const file = fs.readFileSync('./chapter1.flash', 'utf8');
-const data = YAML.parseAllDocuments(file);
+const data = YAML.loadAll(file);
 
 const meta = data.shift();
-const heading = meta.contents.get('heading');
+const heading = meta.shift().heading;
 
 console.log(heading);
 
@@ -34,7 +33,7 @@ for (let i = 0; i < data.length; i++) {
 
   const description = document.createElement('td');
   description.className = 'description';
-  description.textContent = meta.contents.get('description');
+  description.textContent = meta.description;
 
   row.appendChild(description);
 }
