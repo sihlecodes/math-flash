9e1cab8e91522e0d3b7965e50e225cbf7fa21c2b
Siphesihle Mhlongo <sihlecodes@gmail.com>
2025-09-05 06:51:45 +0200

    Implement simple batch flashcard reading

--- index.js
+++ index.js
@@ -4,26 +4,54 @@ const fs = require('fs');
 
 const YAML = require('yaml');
 
+const DOM = require('fauxdom');
+
+const document = new DOM('<!DOCTYPE html><head><style>td { border: 1px solid black; }</style></head><body></body>');
+const body = document.getElementsByTagName('body')[0];
+
+const columns = 2, rows = 4;
+
+const flashCardFronts = document.createElement('table');
+
 const file = fs.readFileSync('./chapter1.flash', 'utf8');
 const data = YAML.parseAllDocuments(file);
 
-let fronts = [
-];
+const meta = data.shift();
+const heading = meta.contents.get('heading');
 
-let backs = [
-];
+console.log(heading);
 
-for (const d of data) {
-  console.log(d.contents);
+let row = document.createElement('tr');
+
+for (let i = 0; i < data.length; i++) {
+  const meta = data[i]; // YAML document
+
+  if (i % 2 == 0 && i > 0) {
+    console.log(row.outerHTML, '\n');
+    flashCardFronts.appendChild(row);
+    row = document.createElement('tr');
+  }
+
+  const description = document.createElement('td');
+  description.className = 'description';
+  description.textContent = meta.contents.get('description');
+
+  row.appendChild(description);
 }
+
+flashCardFronts.appendChild(row);
+body.appendChild(flashCardFronts);
+
+console.log(document.innerHTML);
+
+fs.writeFileSync('test.html', document.innerHTML);
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(`file:${path.join(__dirname, 'public', 'index.html')}`, {
-    waitUntil: 'networkidle0', // Waits until no network connections for 500ms
-  });
+  const index = path.join(__dirname, 'public', 'index.html')
+  await page.goto(`file:${index}`, { waitUntil: 'networkidle0', });
 
   await page.pdf({
     path: 'output.pdf',
