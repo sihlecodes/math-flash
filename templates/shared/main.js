532c3f8fd32412757060c0a6a0d5206543e33ec4
Siphesihle Mhlongo <sihlecodes@gmail.com>
2025-09-12 16:59:35 +0200

    Added katex macros

--- templates/shared/main.js
+++ templates/shared/main.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
   renderMathInElement(document.body, {
     delimiters: [
-      {left: "$$", right: "$$", display: true},
-      {left: "\\[", right: "\\]", display: true},
-      {left: "$", right: "$", display: false}
+      {left: '$$', right: '$$', display: true},
+      {left: '\\[', right: '\\]', display: true},
+      {left: '$', right: '$', display: false}
     ],
     macros: {
-      "\\N": "\\mathbb{N}",
-      "\\Z": "\\mathbb{Z}",
-      "\\Q": "\\mathbb{Q}",
-      "\\R": "\\mathbb{R}",
-      "\\C": "\\mathbb{C}",
-      "\\te": "\\exist",
-      "\\fa": "\\forall",
-      "\\t": "\\enskip\\text{#1}\\enskip",
-      "\\Re": "\\thinspace\\text{Re}\\thinspace #1",
-      "\\Im": "\\thinspace\\text{Im}\\thinspace #1",
+      // aliases
+      '\\N': '\\mathbb{N}',
+      '\\Z': '\\mathbb{Z}',
+      '\\Q': '\\mathbb{Q}',
+      '\\R': '\\mathbb{R}',
+      '\\C': '\\mathbb{C}',
+      '\\te': '\\exist',
+      '\\fa': '\\forall',
+      '\\inv': '^{-1}',
+
+      // new operators
+      '\\t': '\\enskip\\text{#1}\\enskip',
+      '\\Re': '\\thinspace\\text{Re}\\thinspace #1',
+      '\\Im': '\\thinspace\\text{Im}\\thinspace #1',
+      '\\lcm': '\\text{lcm}',
+      '\\equivm': '\\thinspace\\equiv{\\thinspace #1}\\pmod #2',
     }
   })
 });
