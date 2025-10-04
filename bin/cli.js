5fc28223cb14ba35ed37f243fc0880df46e74d33
Siphesihle Mhlongo <sihlecodes@gmail.com>
2025-10-04 15:59:35 +0200

    Added / updated cli flags

--- bin/cli.js
+++ bin/cli.js
@@ -16,8 +16,8 @@ parser.add_argument('-V', '--version', { action: 'version', version: require('..
 parser.description = 'A tool for generating printable math flash cards from a yaml definition file.';
 
 const xorGroup = parser.add_mutually_exclusive_group();
-xorGroup.add_argument('--html-only', { action: 'store_true' });
-xorGroup.add_argument('--pdf-only', { action: 'store_true' });
+xorGroup.add_argument('-H', '--html-only', { action: 'store_true' });
+xorGroup.add_argument('-P', '--pdf-only', { action: 'store_true' });
 
 const group = parser.add_argument_group({ title: 'export options' });
 group.add_argument('-f', '--format', { default: 'A4', help: 'pdf output format; defaults to A4. (note: ignored if --html-only is used along with this option.)' });
@@ -36,7 +36,7 @@ liveGroup.add_argument('-p', '--port', { default: 3000 });
 liveGroup.add_argument('-I', '--check-interval', { default: 100, help: 'interval for checking flash file changes (in milliseconds)' });
 liveGroup.add_argument('-N', '--no-open', { action: 'store_true', help: 'start the preview server without opening the browser.' });
 
-parser.add_argument('-t', '--template', { default: 'view-all.ejs' })
+parser.add_argument('-t', '--template', { default: 'print' })
 parser.add_argument('-g', '--generate', { action: 'store_true', help: 'create a new flash card file using the default template.yaml' })
 parser.add_argument('flash_card_file', { metavar: 'FLASH_CARD_FILE', help: 'YAML file containing flash card definitions' });
 
